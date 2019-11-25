68346a9f5204e4092d1b9cc0655da68feb128aab
Karl Hulme <karlhulme@hotmail.com>
2019-11-25 12:20:43 +0000

    Trying to fix input issue

--- src/run.js
+++ src/run.js
@@ -1,18 +1,18 @@
-const core = require('@actions/core')
+// const core = require('@actions/core')
 // const { GitHub, context } = require('@actions/github')
 
 const run = async () => {
   // const { owner } = context.repo
   console.log('start')
-  const authorisedLogins = core.getInput('authorisedLogins').split(',')
-  const comment = core.getInput('comment')
+  // const authorisedLogins = core.getInput('authorisedLogins').split(',')
+  // const comment = core.getInput('comment')
 
-  const author = (comment.user || {}).login
-  console.log(`Author: ${author}`)
-  const isAuthorisedUser = authorisedLogins.includes(author)
-  console.log(`isAuthUser: ${isAuthorisedUser}`)
+  // const author = (comment.user || {}).login
+  // console.log(`Author: ${author}`)
+  // const isAuthorisedUser = authorisedLogins.includes(author)
+  // console.log(`isAuthUser: ${isAuthorisedUser}`)
 
-  console.log(`Authorised logins '${authorisedLogins.join('|')}' wrote: ${comment.body}`)
+  // console.log(`Authorised logins '${authorisedLogins.join('|')}' wrote: ${comment.body}`)
 }
 
 module.exports = run
