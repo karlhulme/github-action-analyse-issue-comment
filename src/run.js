6643370b5674515ad8cd9fa41e9f99fbbff4d5a5
Karl Hulme <karlhulme@hotmail.com>
2019-11-25 12:30:46 +0000

    Log inputs to console.

--- src/run.js
+++ src/run.js
@@ -1,12 +1,15 @@
-// const core = require('@actions/core')
-// const { GitHub, context } = require('@actions/github')
+const core = require('@actions/core')
 
 const run = async () => {
-  // const { owner } = context.repo
   console.log('start')
-  // const authorisedLogins = core.getInput('authorisedLogins').split(',')
-  // const comment = core.getInput('comment')
+  const authorisedLogins = core.getInput('authorisedLogins')
+  console.log(authorisedLogins)
+  const commentAuthor = core.getInput('commentAuthor')
+  console.log(commentAuthor)
+  const commentBody = core.getInput('commentBody')
+  console.log(commentBody)
 
+  core.setOutput('instruction', 'dive_into_snow')
   // const author = (comment.user || {}).login
   // console.log(`Author: ${author}`)
   // const isAuthorisedUser = authorisedLogins.includes(author)
